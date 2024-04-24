66c90d222aa214ce096d49c4e28d830464946de0
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-04-24 09:43:42 +0300

    1 mon

---
+++ js/JS task-2.js
@@ -0,0 +1,15 @@
+ 
+ let country; 
+ let price;
+ let deliveryFee;
+ let totalPrice = `${price + deliveryFee}`; 
+
+ function getShippingMessage(country, price, deliveryFee)
+  { return`"Shipping to ${country} will cost ${price + deliveryFee} credits"`};
+
+    console.log(getShippingMessage("Australia", 120, 50));
+    
+    console.log(getShippingMessage("Germany", 80, 20));
+    
+    console.log(getShippingMessage("Sweden", 100, 20)); 
+    
---
+++ js/JS task-3.js
@@ -0,0 +1,27 @@
+
+ let content;
+ let padding;
+ let border;
+ console.log(typeof content);
+ console.log(typeof padding);
+ console.log(typeof border);
+ console.log(isNaN(content));
+ console.log(isNaN(padding));
+ console.log(isNaN(border));
+ 
+ let totalP = `${padding * 2}`; 
+ let totalPr = `${border * 2}`;
+
+ function getElementWidth(content, padding, border) {
+    return `content + ${padding * 2} + ${border * 2}`
+ };
+
+ 
+ console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+// це рядки формату Npx?const Npx (content, padding, border); content = Npx(content);
+ //padding = Npx(padding);
+ //border = Npx(border);
+ //console.log(Number(content));
