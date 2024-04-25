5065d947b84e2f72ed61d8244597b4bbea55f537
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-04-25 12:23:55 +0300

    1

--- js/JS task-3.js
+++ js/JS task-3.js
@@ -1,6 +1,26 @@
 
- let content;
- let padding;
+let content;
+let padding;
+let border;
+ 
+
+ function getElementWidth(content, padding, border) {
+   
+   
+   return Number(Number.parseFloat[content]) + Number(Number.parseFloat[padding]) + Number(Number.parseFloat[border]);
+   
+}
+ 
+ console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+// це рядки формату Npx?const Npx (content, padding, border); content = Npx(content);
+ //padding = Npx(padding);
+ //border = Npx(border);
+ //console.log(Number(content));
+ //let content;
+ /*let padding;
  let border;
  console.log(typeof content);
  console.log(typeof padding);
@@ -14,14 +34,8 @@
 
  function getElementWidth(content, padding, border) {
     return `content + ${padding * 2} + ${border * 2}`
- };
-
- 
- console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
-
-// це рядки формату Npx?const Npx (content, padding, border); content = Npx(content);
- //padding = Npx(padding);
- //border = Npx(border);
- //console.log(Number(content));
+ };*/
+ //const Number = content.parseFloat();
+ /*const Number = padding.parseFloat();
+ const Number = border.parseFloat();*/
+ // console.log(typeof Number.parseFloat);
